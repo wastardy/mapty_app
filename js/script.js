a06eeb34600e89aedc90e25dddf07fbc60519525
Andrew Kononenko <wastardy.k@gmail.com>
2025-02-06 20:21:32 +0200

    add init files

---
+++ js/script.js
@@ -0,0 +1,35 @@
+'use strict';
+
+//#region Query Selectors
+const months = [
+    'January', 
+    'February', 
+    'March', 
+    'April', 
+    'May', 
+    'June', 
+    'July', 
+    'August', 
+    'September', 
+    'October', 
+    'November', 
+    'December'
+];
+
+const form = document.querySelector('.form');
+const containerWorkouts = document.querySelector('.workouts');
+const inputType = document.querySelector('.form__input--type');
+const inputDistance = document.querySelector('.form__input--distance');
+const inputDuration = document.querySelector('.form__input--duration');
+const inputCadence = document.querySelector('.form__input--cadence');
+const inputElevation = document.querySelector('.form__input--elevation');
+//#endregion
+
+//#region Methods
+//#endregion
+
+//#region Event Handlers
+//#endregion
+
+//#region Testing
+//#endregion
