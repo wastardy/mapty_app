22c954e2933aba235611c1d9c7bfa4675157084e
Andrew Kononenko <wastardy.k@gmail.com>
2025-02-15 16:12:44 +0200

    add _renderWorkout method to show activity details in form

--- js/script.js
+++ js/script.js
@@ -179,6 +179,59 @@ class App {
             .openPopup();
     }
 
+    _renderWorkout(workout) {
+        let html = `
+            <li class="workout workout--${workout.type}" data-id="${workout.id}">
+                <h2 class="workout__title">${workout.description}</h2>
+                <div class="workout__details">
+                    <span class="workout__icon">
+                        ${workout.type === 'running' ? '🏃‍♂️' : '🚴‍♀️'}
+                    </span>
+                    <span class="workout__value">${workout.distance}</span>
+                    <span class="workout__unit">km</span>
+                </div>
+                <div class="workout__details">
+                    <span class="workout__icon">⏱</span>
+                    <span class="workout__value">${workout.duration}</span>
+                    <span class="workout__unit">min</span>
+                </div>
+        `;
+
+        if (workout.type === 'running') {
+            html += `
+                    <div class="workout__details">
+                        <span class="workout__icon">⚡️</span>
+                        <span class="workout__value">${workout.pace}</span>
+                        <span class="workout__unit">min/km</span>
+                    </div>
+                    <div class="workout__details">
+                        <span class="workout__icon">🦶🏼</span>
+                        <span class="workout__value">${workout.cadence}</span>
+                        <span class="workout__unit">spm</span>
+                    </div>
+                </li>
+            `;
+        }
+
+        if (workout.type === 'cycling') {
+            html += `
+                <div class="workout__details">
+                    <span class="workout__icon">⚡️</span>
+                    <span class="workout__value">${workout.speed}</span>
+                    <span class="workout__unit">km/h</span>
+                </div>
+                <div class="workout__details">
+                    <span class="workout__icon">⛰</span>
+                    <span class="workout__value">${workout.elevationGain}</span>
+                    <span class="workout__unit">m</span>
+                </div>
+            </li>
+            `;
+        }
+
+        form.insertAdjacentHTML('afterend', html);
+    }
+
     _newWorkout(event) {
         const checkIsNumberInputs = (...inputs) => 
             inputs.every(input => Number.isFinite(input));
